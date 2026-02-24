0e10fa2b8b29d4fe54c89f1bc91cc8dc4ca90380
Andreas Brem <andi.brem@web.de>
2026-02-24 11:34:18 +0100

    feat: enhance project tab functionality and add smooth scrolling effect
    - Implemented showProject function to manage project tab display.
    - Created showHeaderTab and showContentTab functions for better tab management.
    - Added scrollToTarget function for smooth scrolling to target sections.
    - Introduced bounceWholePage function for visual feedback on scroll completion.
    - Updated CSS styles for buttons, sections, and added new styles for references and contact sections.
    - Improved layout and responsiveness of project content and references sections.

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 
 
+function showProject(ev) {
+    let projectTabs = ev.target.dataset.project;
+    let contentTabs = ev.target.dataset.content;
+    let targetId = ev.target.dataset.target;
+    showHeaderTab(projectTabs, ev);
+    showContentTab(contentTabs, targetId);
 
+}
 
-function showTab(name) {
-    let tab = document.getElementsByClassName('project-content-tab');
+function showHeaderTab(projectTabs, ev) {
+    let tab = document.getElementsByClassName(projectTabs);
+    for (let i = 0; i < tab.length; i++) {
+        tab[i].classList.remove("active");
+        tab[i].ariaSelected = "false";
+    }
+    ev.target.parentElement.classList.add("active");
+    ev.target.parentElement.ariaSelected = "true";
+}
+
+function showContentTab(contentTabs, targetId) {
+    let tab = document.getElementsByClassName(contentTabs);
     for (let i = 0; i < tab.length; i++) {
         tab[i].style.display = "none";
         tab[i].ariaSelected = "false";
     }
-    document.getElementById(name).style.display = "block";
-    document.getElementById(name).ariaSelected = "true";
+    document.getElementById(targetId).style.display = "flex";
+    document.getElementById(targetId).ariaSelected = "true";
+}
+
+function scrollToTarget(targetId, ev) {
+    ev.preventDefault();
+    let target = document.getElementById(targetId);
+    target.scrollIntoView({ behavior: "smooth" })
+    window.addEventListener("scrollend", () => {
+        bounceWholePage();
+    }, {once: true});
+}
+
+function bounceWholePage() {
+    let main = document.querySelector("main");
+    main.classList.add("bounce-page");
+    setTimeout(() => {
+        main.classList.remove("bounce-page");
+    }, 500);
 }
