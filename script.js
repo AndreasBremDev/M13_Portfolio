dd761fda20256b0b7735f17b7a21660984690a27
Andreas Brem <andi.brem@web.de>
2026-02-28 12:38:23 +0100

    feat: update SVG naming, enhance hero section styles, and improve responsive design, added textAnimation in me-section

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function toggleClassActive(ev, tabs) {
         tab[i].setAttribute("aria-selected", "false");
     }
     let targetElement = ev.currentTarget;
-    if (targetElement.dataset.project){
+    if (targetElement.dataset.project) {
         targetElement.parentElement.classList.add("active");
         targetElement.parentElement.setAttribute("aria-selected", "true");
     } else {
@@ -57,4 +57,159 @@ function bounceWholePage() {
     setTimeout(() => {
         main.classList.remove("bounce-page");
     }, 300);
+}
+
+let animationMeLocation = [
+    '<img src="./assets/img/me_located_1.png" alt="icon showing a located needle">',
+    '<span class="color-lightblue">I&nbsp;</span>',
+    '<span class="color-lightblue">a</span>',
+    '<span class="color-lightblue">m&nbsp;</span>',
+    '<span>l</span>',
+    '<span>o</span>',
+    '<span>c</span>',
+    '<span>a</span>',
+    '<span>t</span>',
+    '<span>e</span>',
+    '<span>d</span>',
+    '<span>&nbsp;</span>',
+    '<span>i</span>',
+    '<span>n&nbsp;</span>',
+    '<span>M</span>',
+    '<span>u</span>',
+    '<span>n</span>',
+    '<span>i</span>',
+    '<span>c</span>',
+    '<span>h&nbsp;</span>',
+    '<span class="color-lightblue">.</span>',
+    '<span class="color-lightblue">.</span>',
+    '<span class="color-lightblue">.</span>'
+];
+let animationMeHybrid = [
+    '<img src="./assets/img/me_remote.png" alt="icon PC indicating remote work">',
+    '<span class="color-lightblue">I&nbsp;</span>',
+    '<span class="color-lightblue">a</span>',
+    '<span class="color-lightblue">m&nbsp;</span>',
+    '<span>o</span>',
+    '<span>p</span>',
+    '<span>e</span>',
+    '<span>n&nbsp;</span>',
+    '<span>t</span>',
+    '<span>o&nbsp;</span>',
+    '<span>w</span>',
+    '<span>o</span>',
+    '<span>r</span>',
+    '<span>k&nbsp;</span>',
+    '<span>h</span>',
+    '<span>y</span>',
+    '<span>b</span>',
+    '<span>r</span>',
+    '<span>i</span>',
+    '<span>d&nbsp;</span>',
+    '<span class="color-lightblue">.</span>',
+    '<span class="color-lightblue">.</span>',
+    '<span class="color-lightblue">.</span>'
+];
+let animationBlinkingCursor = [
+    '<span class="color-lightblue">&#124;</span>'
+];
+let meInteractive = document.getElementById('meInteractive');
+let isVisible_meInteractive = false;
+let isRunningAnimation = false;
+
+let observerMeInteractive = new IntersectionObserver((entries) => {
+    entries.forEach(async entry => {
+        if (entry.isIntersecting) {
+            isVisible_meInteractive = true;
+            if (!isRunningAnimation) {
+                isRunningAnimation = true;
+                await runAnimationSequence();
+                isRunningAnimation = false;
+            }
+        } else {
+            isVisible_meInteractive = false;
+        }
+    });
+});
+
+observerMeInteractive.observe(meInteractive);
+
+async function runAnimationSequence() {
+    try {
+        while (isVisible_meInteractive) {
+            await writeAnimation(animationMeLocation);
+            if (!isVisible_meInteractive) return;
+            await blinkingCursor(animationMeLocation, animationBlinkingCursor);
+            if (!isVisible_meInteractive) return;
+            await eraseAnimation(animationMeLocation);
+            if (!isVisible_meInteractive) return;
+            await writeAnimation(animationMeHybrid);
+            if (!isVisible_meInteractive) return;
+            await blinkingCursor(animationMeHybrid, animationBlinkingCursor);
+            if (!isVisible_meInteractive) return;
+            await eraseAnimation(animationMeHybrid);
+        }
+    } catch (err) {
+        console.error('current error: ', err);
+    }
+}
+
+function writeAnimation(arr) {
+    return new Promise((resolve) => {
+        let index = 0;
+        let writingInterval = setInterval(() => {
+            if (checkIfVisibleAndStop(writingInterval, resolve)) return;
+            meInteractive.innerHTML = arr.slice(0, index + 1).join('');
+            index++;
+            if (index >= arr.length) {
+                clearInterval(writingInterval);
+                resolve();
+            }
+        }, 125);
+    });
+}
+
+function blinkingCursor(arrMain, arrBlink, times = 3) {
+    return new Promise((resolve) => {
+        let showCursor = true;
+        let counter = 0;
+        let blinkingInterval = setInterval(() => {
+            if (checkIfVisibleAndStop(blinkingInterval, resolve)) return;
+            meInteractive.innerHTML = '';
+            meInteractive.innerHTML = showCursor ? (arrMain.join('') + arrBlink.join('')) : arrMain.join('');
+            showCursor = !showCursor;
+            counter++;
+            stopBlinkingIfComplete(counter, times, blinkingInterval, resolve);
+        }, 400);
+    });
+}
+
+function stopBlinkingIfComplete(counter, times, blinkingInterval, resolve) {
+    if (counter >= times * 2) {
+        clearInterval(blinkingInterval);
+        resolve();
+    }
+}
+
+function eraseAnimation(arr) {
+    return new Promise((resolve) => {
+        let index = arr.length;
+        let blup = setInterval(() => {
+            if (checkIfVisibleAndStop(blup, resolve)) return;
+            meInteractive.innerHTML = arr.slice(0, index).join('');
+            index--;
+            if (index < 1) {
+                clearInterval(blup);
+                resolve();
+            }
+        }, 40);
+    });
+}
+
+function checkIfVisibleAndStop(intervalId, resolve) {
+    if (!isVisible_meInteractive) {
+        clearInterval(intervalId);
+        resolve();
+        return true;
+    }
+    return false;
 }
